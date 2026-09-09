a753b71c97de71ca95fd0030003bdb667d34a8ce
mariabrancorocha-hash <maria.branco.rocha@escola.pr.gov.br>
2026-09-09 13:42:07 -0300

    atualizado

--- Script.js
+++ Script.js
@@ -1,63 +1,18 @@
-// ===============================
-// BOTÕES "SAIBA MAIS"
-// ===============================
-
-const botoesSaibaMais = document.querySelectorAll(".botao-saiba-mais");
-
-botoesSaibaMais.forEach(function (botao) {
-
-    botao.addEventListener("click", function () {
-
-        const informacoes = botao.nextElementSibling;
-
-        informacoes.classList.toggle("aberta");
-
-        if (informacoes.classList.contains("aberta")) {
-            botao.textContent = "Mostrar menos 🌷";
-        } else {
-            botao.textContent = "Saiba mais 🌷";
-        }
-
-    });
-
-});
-
-
-// ===============================
-// BOTÕES DE REAÇÃO
-// ===============================
+alert("O JavaScript está funcionando!");
 
 const botoesReacao = document.querySelectorAll(".reacao");
 
 botoesReacao.forEach(function (botao) {
 
-    let curtiu = false;
-
     botao.addEventListener("click", function () {
 
         const contador = botao.querySelector("span");
 
-        let quantidade = Number(contador.textContent);
+        let numero = Number(contador.textContent);
 
-        if (!curtiu) {
+        numero = numero + 1;
 
-            quantidade++;
-
-            curtiu = true;
-
-            botao.classList.add("ativo");
-
-        } else {
-
-            quantidade--;
-
-            curtiu = false;
-
-            botao.classList.remove("ativo");
-
-        }
-
-        contador.textContent = quantidade;
+        contador.textContent = numero;
 
     });
 
